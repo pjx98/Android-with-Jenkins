8b9a03f8c7a86a08007fd1df56506a478e8bfa9d
PJX <pjingxiang@gmail.com>
2021-01-15 18:49:02 +0800

    Jenkins

--- app/src/test/java/com/example/convenienestudy/ExampleUnitTest.java
+++ app/src/test/java/com/example/convenienestudy/ExampleUnitTest.java
@@ -20,12 +20,6 @@ import static org.junit.Assert.*;
  */
 
 public class ExampleUnitTest {
-    
-
-    @Test
-    public void addition_isCorrect() {
-        assertEquals(4, 2 + 2);
-    }
 
     @Test
     public void add_question(){
